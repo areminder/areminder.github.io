54a176156f7c6d814c3b8cb20636adbde076aca0
gabrielbreno <gabrielbrenoa@gmail.com>
2016-11-03 18:03:09 -0200

    Correções no index público e alteração do layout.

--- areminder/web/assets/vendor/dist/js/main.js
+++ areminder/web/assets/vendor/dist/js/main.js
@@ -62,10 +62,10 @@ $(document).ready(function(){
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
+            $(".navbar-brand a").css("color","#333333");
             $("#top-bar").removeClass("animated-header");
         } else {
-            $(".navbar-brand a").css("color","inherit");
+            $(".navbar-brand a").css("color","#333333");
             $("#top-bar").addClass("animated-header");
         }
     });

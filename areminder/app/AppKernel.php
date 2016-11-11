a7d82a358593c6f543745ff4c6099372eb4ffb7c
gabrielbreno <gabrielbrenoa@gmail.com>
2016-11-11 18:06:17 -0200

    Instalação do Assetic Bundle.

--- areminder/app/AppKernel.php
+++ areminder/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new MainBundle\MainBundle(),
             new AppBundle\AppBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

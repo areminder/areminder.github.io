45be5f0e7ab8745a689ac9a84bd4cabc2548884c
gamoridev <gabrielbrenoa@gmail.com>
2016-11-18 23:36:42 -0200

    Alterações no visao-geral

--- areminder/src/VisaoIBundle/Controller/VisaoIController.php
+++ areminder/src/VisaoIBundle/Controller/VisaoIController.php
@@ -4,6 +4,9 @@ namespace VisaoIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use VisaoIBundle\Entity\Tarefas;
+use VisaoIBundle\Entity\Notas;
+use VisaoIBundle\Entity\Provas;
 
 class VisaoIController extends Controller
 {
@@ -12,6 +15,25 @@ class VisaoIController extends Controller
 	*/
     public function indexAction()
     {
-        return $this->render('VisaoIBundle:Default:visao.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+
+    	$user = $this->get('security.token_storage')->getToken()->getUser();
+
+    	/**
+    	* Traz todas as tarefas não finalizadas do usuário logado.
+    	*/
+    	$tarefas_pendentes = $em->getRepository('VisaoIBundle:Tarefas')->findByUser($user->getId());
+    	$tarefas_pendentes = $em->getRepository('VisaoIBundle:Tarefas')->findByFinalizada(false);
+    	$notas = $em->getRepository('VisaoIBundle:Notas')->findByUser($user->getId());
+    	$provas = $em->getRepository('VisaoIBundle:Provas')->findByUser($user->getId());
+
+    	/**
+    	* Retorna para a view, o array de tarefas selecionadas anteriormente.
+    	*/
+    	return $this->render('VisaoIBundle:Default:visao.html.twig', array(
+            'tarefasP' => $tarefas_pendentes,
+            'notas' => $notas,
+            'provas' => $provas
+        ));
     }
 }
